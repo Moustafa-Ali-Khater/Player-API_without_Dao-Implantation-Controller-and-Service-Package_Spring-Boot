26c1198af9e1886e4fa60242178d84330f80b4be
EslamKhder <eslamkhder81@gmail.com>
2021-05-18 23:28:10 +0200

    lombok & model player

---
+++ src/main/java/com/spring/player/model/Player.java
@@ -0,0 +1,40 @@
+package com.spring.player.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "players")
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "player_id")
+    private int id;
+
+    @Column(name = "player_name")
+    private String name;
+
+    @Column(name = "player_number")
+    private int number;
+
+    @Column(name = "player_address")
+    private String address;
+
+    @Column(name = "player_phone")
+    private String phone;
+
+}
