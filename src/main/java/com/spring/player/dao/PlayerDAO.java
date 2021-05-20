389cc7cfe789a2d63e1a87953607c7a33fcba011
EslamKhder <eslamkhder81@gmail.com>
2021-05-20 02:47:04 +0200

    spring-boot-starter-data-rest

--- src/main/java/com/spring/player/dao/PlayerDAO.java
+++ src/main/java/com/spring/player/dao/PlayerDAO.java
@@ -5,5 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlayerDAO extends JpaRepository<Player,Integer> {
 
-
 }
+
+//   Player
+/*
+============> http://localhost:8080/
+
+Get      http://localhost:8080/players
+Get      http://localhost:8080/players/id
+Post     http://localhost:8080/players      body Player
+Put      http://localhost:8080/players      body Player
+Delete   http://localhost:8080/players/id
+
+
+* */
